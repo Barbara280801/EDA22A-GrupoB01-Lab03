8967cd92999a12f66ee4159d940a8a4736d192db
bryanhancco <bhanccoco@unsa.edu.pe>
2022-06-04 12:09:12 -0500

    Modificando el main

--- Ejercicio01/SameStack.java
+++ Ejercicio01/SameStack.java
@@ -1,13 +1,20 @@
 public class SameStack {
 	public static void main(String[] args) {
-		// Sección de pruebas
-		int[] h1 = {1, 1, 1, 2, 3};
-		int[] h2 = {2, 3, 4};
-		int[] h3 = {1, 4, 1, 1};
+		// Sección de prueba
+		int[] h1 = {10, 1, 1, 2, 3, 3, 5, 6};
+		int[] h2 = {2, 3, 4, 3, 1, 4, 9};
+		int[] h3 = {3, 2, 4, 3, 7, 1, 1, 1};
 		
 		int maxHeight = equalStacks(h1, h2, h3);
-		System.out.println(maxHeight);
+		imprimir(arrHeights(h1), arrHeights(h2), arrHeights(h3), maxHeight);
 	} 
+	public static void imprimir(int[] h1, int[] h2, int[] h3, int maxH) {
+		System.out.println("La altura máxima es " + maxH);
+		System.out.println("Las alturas se reducen de la siguiente manera:");
+		System.out.println("\t" + (h1[h1.length - 1]) + " - " + (h1[h1.length - 1] - maxH) + " = " + maxH);
+		System.out.println("\t" + (h2[h2.length - 1]) + " - " + (h2[h2.length - 1] - maxH) + " = " + maxH);
+		System.out.println("\t" + (h3[h3.length - 1]) + " - " + (h3[h3.length - 1] - maxH) + " = " + maxH);
+	}
 	public static int equalStacks(int[] h1, int[] h2, int[] h3) {
 		int[] sums = new int[3];
 		int[] arr1 = arrHeights(h1); 
@@ -20,7 +27,6 @@ public class SameStack {
 				if (n2 == n1) {
 					for (int n3 : arr3) {
 						if (n1 < n3) break;
-						System.out.println(n1 + " - " + n2 + " - " + n3);
 						if (n3 == n1)
 							maxHeight = n1;
 					}
