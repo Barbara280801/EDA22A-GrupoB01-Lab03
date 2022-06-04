ebc21eea1aa67fc859795a3fb052a9eeea648695
bryanhancco <bhanccoco@unsa.edu.pe>
2022-06-04 10:46:32 -0500

    Agregando datos iniciales

--- Ejercicio01/SameStack.java
+++ Ejercicio01/SameStack.java
@@ -1,5 +1,11 @@
+import java.util
 public class SameStack {
-	public static int equalStacks(int[] h1, int h2, int h3) {
-
+	public static int equalStacks(int[] h1, int[] h2, int[] h3) {
+		int[] sums = new int[3];
+		for (int n1 : h1) {
+			for (int n2: h2) {
+				for (int n3: h3)
+			}
+		}
 	}
 }
