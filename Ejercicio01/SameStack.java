9bec7d681ae77933dbf3983204eb0cac57164424
bryanhancco <bhanccoco@unsa.edu.pe>
2022-06-04 11:20:05 -0500

    Agregando funcionalidad, corrigiendo errores

--- Ejercicio01/SameStack.java
+++ Ejercicio01/SameStack.java
@@ -1,10 +1,19 @@
 public class SameStack {
+	public static void main(String[] args) {
+		// Sección de pruebas
+		int[] h1 = {1, 1, 1, 2, 3};
+		int[] h2 = {2, 3, 4};
+		int[] h3 = {1, 4, 1, 1};
+		
+		int maxHeight = equalStacks(h1, h2, h3);
+		System.out.println(maxHeight);
+	} 
 	public static int equalStacks(int[] h1, int[] h2, int[] h3) {
 		int[] sums = new int[3];
 		int[] arr1 = arrHeights(h1); 
 		int[] arr2 = arrHeights(h1); 
 		int[] arr3 = arrHeights(h1); 
-		int maxHeigth;
+		int maxHeight = 0;
 		for (int n1 : arr1) {
 			for (int n2 : arr2) {		
 				if (n1 < n2) break;
@@ -12,14 +21,14 @@ public class SameStack {
 					for (int n3 : arr3) {
 						if (n1 < n3) break;
 						if (n3 == n1)
-							maxHeigth = n1;
+							maxHeight = n1;
 					}
 				}
 			}
 		}
-		return maxHeigth;
+		return maxHeight;
 	}
-	public static int[] arrHeigths(int[] h) {
+	public static int[] arrHeights(int[] h) {
 		int[] heightsArr = new int[h.length];
 		int sum = 0;
 		for (int i = 0; i < h.length; i++) {
