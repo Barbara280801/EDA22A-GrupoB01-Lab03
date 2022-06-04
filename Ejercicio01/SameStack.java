da6568ffc5fbe6d396ffdc0ee0dfb357ade9f9e3
bryanhancco <bhanccoco@unsa.edu.pe>
2022-06-04 11:15:11 -0500

    Haciendo que los bucles realicen comparaciones continuamente

--- Ejercicio01/SameStack.java
+++ Ejercicio01/SameStack.java
@@ -1,16 +1,23 @@
 public class SameStack {
 	public static int equalStacks(int[] h1, int[] h2, int[] h3) {
 		int[] sums = new int[3];
-		int maxHeigth;
 		int[] arr1 = arrHeights(h1); 
 		int[] arr2 = arrHeights(h1); 
 		int[] arr3 = arrHeights(h1); 
-		for (int n1 : h1) {
-			for (int n2 : h2) {		
-				for (int n3 : h3) {
+		int maxHeigth;
+		for (int n1 : arr1) {
+			for (int n2 : arr2) {		
+				if (n1 < n2) break;
+				if (n2 == n1) {
+					for (int n3 : arr3) {
+						if (n1 < n3) break;
+						if (n3 == n1)
+							maxHeigth = n1;
+					}
 				}
 			}
 		}
+		return maxHeigth;
 	}
 	public static int[] arrHeigths(int[] h) {
 		int[] heightsArr = new int[h.length];
